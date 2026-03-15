1d788abee75878e4d332bef362d2be44b9e652bd
Marcus Figueiredo <figueiredo@protonmail.com>
2026-03-15 02:32:43 -0300

    chore: remove pnpm in favor of npm

--- packages/infra/init-localstack.sh
+++ packages/infra/init-localstack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Executado pelo LocalStack após "Ready."
+# Executado pelo LocalStack quando o container estiver "saudável"
 set -e
 
 AWS="aws --endpoint-url=http://localhost:4566 --region us-east-1"
